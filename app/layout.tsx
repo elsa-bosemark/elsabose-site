5a9da4c230a4a6838a14adb43c40fc2a5b1aa0ab
elsa-bosemark <elsabosemark@gmail.com>
2023-09-02 10:39:49 -0700

    scroll up button

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import ScrollToTopButton from "@/context/scroll-top-button"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,10 +30,10 @@ export default function RootLayout({
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
+            <ScrollToTopButton />
             {/* <Header /> */}
             {children}
             <Footer />
-
             <Toaster position="top-right" />
             <ThemeSwitch />
           </ActiveSectionContextProvider>
---
+++ context/scroll-top-button.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState, useEffect } from 'react';
+import { IoIosArrowUp } from "react-icons/io";
+
+export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Add a scroll event listener to update the button's visibility
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  // Scroll to the top when the button is clicked
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // Optional: Add smooth scrolling effect
+    });
+  };
+
+  return (
+    <>
+      {isVisible && (
+        <div className='fixed bottom-5 right-5 z-50 mb-20'>
+          <button
+            className=" bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+            onClick={scrollToTop}
+          >
+            <IoIosArrowUp />
+          </button>
+        </div>
+      )}
+    </>
+  );
+}
