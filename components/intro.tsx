b0603ef6deed613fd261d806654be4143cf1c16e
elsa-bosemark <elsabosemark@gmail.com>
2023-09-01 20:52:56 -0700

    fixed img file jpg directly

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import profilepic from "../public/profilepic.JPG";
+import profilepic from "../public/profilepic.jpg";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
