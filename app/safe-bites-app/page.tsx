dd2012fbadc109c85472e350f03cfde72d3249c9
elsa-bosemark <elsabosemark@gmail.com>
2023-09-02 17:12:25 -0700

    spell check

--- app/safe-bites-app/page.tsx
+++ app/safe-bites-app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     <main className=" flex justify-center">
       <BackButton />
       <div className=" flex items-center justify-center flex-col lg:max-w-[50rem] md:max-w-[40rem] sm:max-w-[35rem] xs:max-w-[10rem] px-5">
-        <SectionHeading>Safe Bites App</SectionHeading>
+        <SectionHeading>Safe Bites Mobile App</SectionHeading>
         <p> Was one of two designers and developers of app that tackled the confusing process of finding restaurants dedicated to keeping customers and employees safe from COVID-19. Through our review app, users can find the places that best follow CDC pandemic regulations, while providing resources for users to support local businesses during quarantine.
         </p>
 
@@ -35,7 +35,7 @@ export default function Home() {
             <h1 className="font-bold text-center">Initial</h1>
             <Image
               src={SB1}
-              alt="Screen shot of a light green background and a demo iphone screen with the old app design"
+              alt="Screen shot of a light green background and a demo iphone screen showing the old app design"
               width="500"
               quality="95"
               className="h-50 w-50 rounded border object-cover shadow-xl my-5"
@@ -50,7 +50,7 @@ export default function Home() {
             <h1 className="font-bold text-center">Revised</h1>
             <Image
               src={SB2}
-              alt="Screen shot of a light blue background and a demo iphone screen with the old app design"
+              alt="Screen shot of a light blue background and a demo iphone screen showing the new app design"
               width="500"
               quality="95"
               className="h-50 w-50 rounded border object-cover shadow-xl my-5"
@@ -62,7 +62,7 @@ export default function Home() {
           <p className="mb-3"><span className="font-medium">Timeline:</span> 3 months</p>
           <p className="mb-3"><span className="font-medium">My Role:</span> Project Manager, Designer, Developer </p>
           <p className="mb-3"><span className="font-medium">Goal:</span> Help people find restaurants that follow COVID safety protocols</p>
-          <p className="mb-3"><span className="font-medium">Constraints:</span>Everything was completed virtually (harder to get feedback on work)</p>
+          <p className="mb-3"><span className="font-medium">Constraints:</span>Time -- Finishing app while the need still exists</p>
         </div>
 
         <div className="justify-center flex">
@@ -75,19 +75,19 @@ export default function Home() {
         <div className="flex justify-center my-10">
           <Image
             src={timeline}
-            alt="A line with separating branches and dates. The first branch is marked by March 2020 and reads, Covid effects world, team transitions to social distancing and reflects on the pandemic issues. Next is April 2020 with the blurb, start of SafeBites, born form the community's needs. On july 2020, it reads, winner of youth MIT App Inventor Hackathon. Finally, on October 2020, it says contact restaurants review platforms for possible integration.  "
+            alt="A line with separating branches and dates. The first branch is marked by March 2020 and reads, Covid effects world, team transitions to social distancing and reflects on the pandemic issues. Next is April 2020 with the blurb, beginning of SafeBites, born form the community's needs. On july 2020, it reads, winner of youth MIT App Inventor Hackathon. Finally, on October 2020, it says contact restaurants review platforms for possible integration.  "
             width="750"
             quality="100"
             className="h-50 w-50 rounded border object-cover shadow-xl"
           />
         </div>
 
-        <p>The UX of the project was created under a fast hackathon deadline and was not optimized with feedback.</p>
+        <p>The UX of the project was created under the constraints of the hackathon deadline and was not optimized with feedback due to a lack of time (also needed to build and create a pitch for the app).</p>
 
         <div className="flex justify-center">
           <Image
             src={UX1}
-            alt="Example of the UX of the old app design. Shoes eight screens ranging from home screen to portfolio page."
+            alt="Example UX of the old app design. Shows eight screens ranging from home screen to portfolio page."
             width="700"
             quality="100"
             className="h-50 w-50 rounded border object-cover shadow-xl my-5"
@@ -134,7 +134,7 @@ export default function Home() {
         </div>
 
         <SectionHeading>Revised</SectionHeading>
-        <p>After the hackathon, the team pursued the app development. I became the project manager that helped with the development and design. Most of the 12 team members outreached and sought feedback for the design. I was one of two coders and designers for the project. I also taught workshops on React Native mobile app programming and how to use Figma for the other team members.</p>
+        <p>After the hackathon, the team pursued the app development. I became the project manager that helped with product development and design. Most members of our 12 person team focused on outreach and design feedback. I was one of two coders and designers for the project. I also taught workshops for the other team members on React Native mobile app programming and how to use Figma.</p>
 
         <h1 className="font-bold text-center ">Design Exploration</h1>
         <div className="justify-center flex">
@@ -143,17 +143,17 @@ export default function Home() {
         <G2ImageCard
           srcL={casestudy}
           altL="Image of the UX of Yelp sourced from Lucia Liu's article."
-          descL="1. Researched designs of other restaurants related applications to understand what users were used to/expected. Image sourced from Lucia Liu."
+          descL="1. Researched designs of other restaurants related applications to understand what users were used to/expected. Image sourced from Lucia Liu's medium article."
           srcR={UX2}
           altR="New UX designs for SafeBites with more features on the home page like buttons for delivery, curbside pick up and more."
-          descR="2. Testing new homepage screens to include more functionality than the previous design. The design above was created as a team as we talked though was aspects of the app we thought would be important."
+          descR="2. Testing new homepage screens to include more functionality than the previous design. The design above was created as a team as we talked through the app's most important elements."
         />
         {/* https://uxdesign.cc/using-the-yelp-app-c37f8f7eeb2 */}
 
         <G2ImageCard
           srcL={Yelp}
           altL="Screenshot of the chief of staff's Linkedin profile"
-          descL="3. Met with the chief of staff at Yelp to understand the challenges of launching products in the restaurant industry. We also got feedback on the UX and app business strategy. "
+          descL="3. Met with the chief of staff at Yelp to understand the challenges of launching products in the restaurant industry. We also got feedback on our UX and business strategy. "
           srcR={team}
           altR="Image of the safe bites team profile pics and names underneath"
           descR="4. Team created surveys and outreached to local restaurants to find beta testers for the application."
@@ -191,9 +191,9 @@ export default function Home() {
           <h1 className="font-bold mt-5">Power of MVP</h1>
           <p> When the project initially kicked off, we were racing against the clock with just one week to meet the hackathon deadline. It wasn't much time to build an app from the ground up. To tackle this challenge, our team collaborated closely to determine the essential features for our Minimum Viable Product (MVP). The MVP turned out to be a lifesaver, serving a dual purpose. Not only did it provide us with a functional project to showcase to the MIT judges, but it also allowed people to connect with our concept right from the early stages of development.</p>
           <h1 className="font-bold mt-5">Managing Virtual Work</h1>
-          <p> The app came to life during the COVID lockdown, which meant a whole lot of Zoom sessions. Collaborating virtually brought its own set of pros and cons, and it was a valuable learning experience for me. The key lesson here is all about communication! I found that things worked smoother when people regularly shared their progress, especially in the realm of software development. I started adopting this practice myself because, well, I appreciated it when others did it. Turns out, those quick updates at shorter intervals proved to be a better way of keeping everyone on the same page. Sure, it might have been easier for us since we were a small and tech-savvy team, but it taught me to truly value the concept of sharing work and ideas collaboratively.</p>
+          <p> The app came to life during the COVID lockdown, which meant a whole lot of Zoom sessions. Collaborating virtually brought its own set of pros and cons, and it was a valuable learning experience for me. The key lesson here is all about communication! I found that things worked smoother when people regularly shared their progress, especially in the realm of software development. I started adopting this practice myself because, well, I appreciated when others did it. Turns out, those regular updates were way better at keeping everyone on the same page. Sure, it might have been easier for us since we were a smaller and tech-savvy team, but it taught me to truly value the concept of sharing work and ideas collaboratively.</p>
           <h1 className="font-bold mt-5">The Fleeting Wave of Demand</h1>
-          <p> If I could offer advice to my past self during the development of SafeBites, I would emphasize the importance of thinking long term. Our approach to addressing our users' needs was inherently short-sighted. It was apparent that larger companies like Yelp and Google would eventually incorporate similar features into their products. Our app fulfilled a "spike demand" rather than addressing a longer-term need. Still, I enjoyed working on the project and gained valuable skills in the process. In the future, recognize that my focus should be on creating products with enduring utility, rather than those catering to short-lived trends.</p>
+          <p> If I could offer advice to my past self during the development of SafeBites, I would emphasize the importance of thinking long term. Our approach to addressing our users' needs was inherently short-sighted. It was apparent that larger companies like Yelp and Google would eventually incorporate similar features into their products. Our app fulfilled a "spike demand" rather than addressing a longer-term need. Still, I enjoyed working on the project and gained valuable skills in the process. In the future, I recognize that my focus should be on creating products with enduring utility, rather than catering to short-lived trends.</p>
         </div>
       </div>
     </main>
