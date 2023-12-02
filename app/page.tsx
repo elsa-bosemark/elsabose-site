3ac6f60e9bfc38e5b9b2203c3bc15c2c7fad3ca0
elsa-bosemark <elsabosemark@gmail.com>
2023-12-02 10:09:52 -0800

    update intro text

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/header";
-// import About from "@/components/about";
+import About from "@/components/about";
 // import Contact from "@/components/contact";
-// import Experience from "@/components/experience";
+import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
--- lib/data.ts
+++ lib/data.ts
@@ -36,32 +36,32 @@ export const links = [
   // },
 ] as const;
 
-// export const experiencesData = [
-//   {
-//     title: "Graduated bootcamp",
-//     location: "Miami, FL",
-//     description:
-//       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-//     icon: React.createElement(LuGraduationCap),
-//     date: "2019",
-//   },
-//   {
-//     title: "Front-End Developer",
-//     location: "Orlando, FL",
-//     description:
-//       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-//     icon: React.createElement(CgWorkAlt),
-//     date: "2019 - 2021",
-//   },
-//   {
-//     title: "Full-Stack Developer",
-//     location: "Houston, TX",
-//     description:
-//       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-//     icon: React.createElement(FaReact),
-//     date: "2021 - present",
-//   },
-// ] as const;
+export const experiencesData = [
+  {
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
+    description:
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2019",
+  },
+  {
+    title: "Front-End Developer",
+    location: "Orlando, FL",
+    description:
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
+  },
+] as const;
 
 export const skillsData = [
   "HTML",
@@ -116,7 +116,7 @@ export const projectsData = [
   // },
 ] as const;
 
-// export const projectInfo = [
-//   { makercart: { title: 'Maker Cart' }, safebites: { title: 'Safe Bites App' }, artexhibit:{ title: 'Interactive Art Exhibit' }}
+export const projectInfo = [
+  { makercart: { title: 'Maker Cart' }, safebites: { title: 'Safe Bites App' }, artexhibit:{ title: 'Interactive Art Exhibit' }}
 
-// ] as const ;
+] as const ;
